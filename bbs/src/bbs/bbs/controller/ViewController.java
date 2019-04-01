e6208f8336513e2004a2d950b544d6403ea13648
greygost <ty82lee@gmail.com>
2019-04-01 11:21:35 +0900

    write process remark

--- bbs/src/bbs/bbs/controller/ViewController.java
+++ bbs/src/bbs/bbs/controller/ViewController.java
@@ -35,7 +35,9 @@ public class ViewController {
 		bbsdto.setPassword(password);
 		int r = bbsservice.loginProcess(bbsdto);
 		System.out.println("result : "+r);
-		String view = "/WEB-INF/views/loginSuccess.jsp";	;
+		String view = "/WEB-INF/views/loginSuccess.jsp";
+		
+//		로그인 실패 하면 실패 페이지로 성공하면 해당 로그인유저의 아이디를 세션에 저장한다.
 		if(r == 0) {
 			view = "/WEB-INF/views/loginFail.jsp";
 		}else {
@@ -55,8 +57,10 @@ public class ViewController {
 	public String writeProcess(@RequestParam("title") String getTitle,@RequestParam("content") String getContent,HttpServletRequest request) {
 		bbsdto.setTitle(getTitle);
 		bbsdto.setContent(getContent);
+//		로그인프로세스에서 저장한 로그인 아이디를 불러온다.
 		HttpSession session = request.getSession();
 		String id = (String) session.getAttribute("loginId");
+//		세션에서 불러온 아이디를 작성자 아이디에 셋팅
 		bbsdto.setLoginId(id);
 		bbsservice.writeProcess(bbsdto);
 		
